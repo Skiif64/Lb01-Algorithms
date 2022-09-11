e9afd9431d41ba38b94ff087f8d3081e2354a858
Skiif64 <86207504+Skiif64@users.noreply.github.com>
2022-09-12 00:16:46 +0400

    Добавьте файлы проекта.

---
+++ Algorithms.Cmd/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace Algorithms.Cmd
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var algorithm = new SvenAlgorithm();
+            var result = algorithm.GetMaximumExtremumRange(Func, 3, 0.01);
+            Console.WriteLine($"[{result.a}; {result.b}]");
+        }
+
+        static double Func(double x)
+        {
+            var r = Math.Pow(x, 2) - x;
+            var result = Math.Pow(r, 1.0 / 3.0);
+            return result;
+        }
+    }
+}
---
+++ Algorithms/SvenAlgorithm.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Algorithms
+{
+    public class SvenAlgorithm
+    {
+        public SvenAlgorithm()
+        {
+
+        }
+
+        public (double a, double b) GetMaximumExtremumRange(Func<double, double> func, double initialX, double step)
+        {
+            //step1
+            var fx = func(initialX);
+            var fxPosStep = func(initialX + step);
+            var fxNegStep = func(initialX - step);
+            int k = 1;
+            double a = 0.0;
+            double b = 0.0;
+
+            //step2
+            double xK = 0.0;
+            double xKMinus1 = initialX;
+            double xKPlus1 = 0.0;
+            if (fxNegStep <= fx && fx <= fxPosStep)
+            {
+                xK = initialX + step;
+                //To step4
+            }
+            //step3
+            else if (fxNegStep >= fx && fx >= fxPosStep)
+            {
+                xK = initialX - step;
+                step = -step;
+                //To step4
+            }
+            else if(fxNegStep <= fx && fx >= fxPosStep)
+            {
+                a = initialX - step;
+                b = initialX + step;
+                return (a, b);
+            }
+            //step 4-5
+            do
+            {
+                xKPlus1 = xK + Math.Pow(2, k) * step;
+                k++;
+            }
+            while (func(xKPlus1) >= func(xK));            
+
+            //step6
+            
+            if (step > 0)
+            {
+                a = xKMinus1;
+                b = xKPlus1;
+            }
+            else
+            {
+                a = xKPlus1;
+                b = xKMinus1;
+            }
+            return (a, b);
+        }
+    }
+}
